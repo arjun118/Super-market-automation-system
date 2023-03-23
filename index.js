00f608b018f15518cf264330a160dae903b9a275
Chayan <20je0291@cse.iitism.ac.in>
2023-03-23 14:44:51 +0530

    chayan basic layout and stat frontend

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express=require('express');
 const app=express()
 const path = require('path');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
-
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/',(req,res)=>{
     res.render('home')
+})
+app.get('/stat',(req,res)=>{
+    res.render('sales_stat')
 })
 
 app.listen(3000,()=>{

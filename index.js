2b63b79b9eac5b3cbdd67d69310e75c0b29a7dba
Chayan <20je0291@cse.iitism.ac.in>
2023-04-03 21:28:31 +0530

    chayan passport

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express=require('express');
-const app=express()
 const path = require('path');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const mongoose = require('mongoose')
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const Window = require('window');
+const flash = require('connect-flash');
 const window = new Window();
 
 const Bill = require('./models/bill')
+const User = require('./models/user')
 const Item = require('./models/item')
-
+const {isLoggedIn} = require('./middleware')
 
 mongoose.connect('mongodb://127.0.0.1:27017/supermarket',{
     useNewUrlParser: true,
@@ -24,15 +26,49 @@ db.once("open", () => {
 });
 
 
+const app=express()
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended:true}))
 
+const sessionConfig={
+    secret: 'supermarketapp',
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        httpOnly: true,
+        expires: Date.now()+1000*60*60*24*2, //expires after two days
+        maxAge: 1000*60*60*24*2 
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next)=>{
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
 app.get('/',(req,res)=>{
     res.render('home')
 })
+
+app.get('/makeuser',async(req,res)=>{
+    const user = new User({name:'chan',user_type:'Manager',username:'chan'});
+    const newuser = await User.register(user,'chan');
+    res.send(newuser);
+})
+
 app.get('/stat',(req,res)=>{
     res.render('sales_stat')
 })
@@ -42,33 +78,7 @@ app.get('/login',(req,res)=>{
 app.get('/register',(req,res)=>{
     res.render('register')
 })
-app.get('/bill',async(req,res)=>{
-    // var items =[
-    //     {
-    //         "id":"0",
-    //         "name":"cake",
-    //         "qty":"10",
-    //         "price":"20"
-    //     },
-    //     {
-    //         "id":"1",
-    //         "name":"biscuit",
-    //         "qty":"20",
-    //         "price":"10"
-    //     },
-    //     {
-    //         "id":"2",
-    //         "name":"vegetable",
-    //         "qty":"30",
-    //         "price":"50"
-    //     },
-    //     {
-    //         "id":"3",
-    //         "name":"toy",
-    //         "qty":"5",
-    //         "price":"100"
-    //     },
-    // ]
+app.get('/bill',isLoggedIn,async(req,res)=>{
     const items = await Item.find({})
     res.render('bill',{items})
 })
@@ -122,11 +132,16 @@ app.post('/bill',async(req,res)=>{
 
 
 app.post('/register',async(req,res)=>{
-    res.send(req.body)
+    
+    const {name,username,user_type,password}=req.body
+    const user = new User({name,user_type,username});
+    const newuser = await User.register(user,password);
+    res.send(newuser);
 })
 
-app.post('/login',async(req,res)=>{
-    res.send(req.body)
+app.post('/login',passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),async(req,res)=>{
+    req.flash('success', 'welcome back!');
+    res.redirect('/bill')
 })
 
 app.listen(3000,()=>{

310c4cd3f7eb050fc7eceb127630e61dc0acb316
bk167465 <bk167465@gmail.com>
2023-04-08 15:18:45 +0530

    brijesh inventory

--- index.js
+++ index.js
@@ -237,13 +237,12 @@ app.post('/add',async(req,res)=>{
 })
 
 app.post('/updateItems',async(req,res)=>{
+    //return res.send(req.body)
     newitem=req.body;
     const x = await Item.findOneAndUpdate({item_code: parseInt(newitem.i1)},{unit_price:newitem.i3 , quantity:newitem.i4})
     const allDetails = await Item.find({});
     res.render('inventory', { details: allDetails})
 })
-
-
 
 
 

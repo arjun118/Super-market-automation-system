66445235df6f96db179f56e14f5c67a2c7f7fba7
arjun118 <vishnutejach3@gmail.com>
2023-04-11 02:05:34 +0530

    vishnu stats pie chart

--- index.js
+++ index.js
@@ -150,10 +150,29 @@ app.get('/additem',async(req,res)=>{
 app.get('/stat',isLoggedIn,async (req,res)=>{
   var filter=0;
   try{
-    const allsales = await Sales.find({});
+    const allsalesforpie = await Sales.aggregate([
+      {
+        $match:{}
+      },
+      {
+        $group: {
+          _id: "$item_name",
+          total: {
+            $sum: {$multiply:["$quantity","$unit_price"]}
+          },
+        }
+      },
+      {
+        $addFields: {
+          "item_name":"$_id"
+        }
+      }
+    ]);
+    const allsales=await Sales.find({})
     const allDetails = await Item.find({});
-
-    res.render('sales_stat',{ allsales, allDetails, filter });
+    // console.log(allsales
+    // const main=true;
+    res.render('sales_stat',{ allsales, allDetails,allsalesforpie, filter });
   } catch (err) {
     console.error(err);
     res.status(500).send("Internal Server Error");
@@ -163,7 +182,8 @@ app.get('/stat',isLoggedIn,async (req,res)=>{
 app.post('/stat',isLoggedIn,async (req,res)=>{
   var filter=req.body.filter
   try{
-    const allsales = await Sales.find({});
+    const allsales = await Sales.find({})
+    // console.log(allsales)
     const allDetails = await Item.find({});
 
     res.render('sales_stat',{ allsales, allDetails, filter });

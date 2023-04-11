da8f6f274b3e8d6c24ba09176733cacda1daa32a
Chayan <20je0291@cse.iitism.ac.in>
2023-04-11 17:36:55 +0530

    chayan month name

--- index.js
+++ index.js
@@ -182,11 +182,27 @@ app.get('/stat',isLoggedIn,async (req,res)=>{
 app.post('/stat',isLoggedIn,async (req,res)=>{
   var filter=req.body.filter
   try{
-    const allsales = await Sales.find({})
-    // console.log(allsales)
+    const allsalesforpie = await Sales.aggregate([
+      {
+        $match:{}
+      },
+      {
+        $group: {
+          _id: "$item_name",
+          total: {
+            $sum: {$multiply:["$quantity","$unit_price"]}
+          },
+        }
+      },
+      {
+        $addFields: {
+          "item_name":"$_id"
+        }
+      }
+    ]);
+    const allsales=await Sales.find({})
     const allDetails = await Item.find({});
-
-    res.render('sales_stat',{ allsales, allDetails, filter });
+    res.render('sales_stat',{ allsales, allDetails,allsalesforpie, filter });
   } catch (err) {
     console.error(err);
     res.status(500).send("Internal Server Error");
